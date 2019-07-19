a8113e1aeaae14cf0c1bea9f5e1aa78e32323975
Desiree Morris <desiquinncompany@gmail.com>
2019-07-19 15:39:30 -0400

    attempted page pagination, see commented out code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import { Button } from "semantic-ui-react";
 
 
 import Characters from "./components/Characters";
@@ -11,19 +12,30 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [starWarsState, setStarWarsState] = useState([]);
+  // const [next, setNext]= useState("")
+  // const [prev, setPrev] = useState("")
+  // const [page, setPage] = useState("https://henry-mock-swapi.herokuapp.com/api")
 
 
   useEffect( () => {
-    axios.get("https://henry-mock-swapi.herokuapp.com/api")
+    axios.get("https://swapi.co/api/people/?format=json")
     // using mock API because Swapi broke
       .then(response => {
         console.log(response.data.results)
         setStarWarsState(response.data.results)
+        // console.log(response.data.next)
+        // setNext(response.data.next)
+        // console.log(response.data.previous)
+        // setPrev(response.data.previous)
       })
       .catch(error => {
         console.log("The Force is NOT With You", error)
       })
   },[]);
+
+
+
+
 
   console.log(starWarsState)
 
@@ -38,6 +50,20 @@ const App = () => {
       {starWarsState.map( (character, i) => {
       return <Characters character={character} key={i}/> })}
       </div>
+      <div className="Buttonwrapper">
+      <Button
+            attached='bottom'
+            content='Prev'
+            // onClick={setPage(prev)}
+            color="purple"
+          />
+      <Button
+            attached='bottom'
+            content='Next'
+            // onClick={setPage(next)}
+            color="teal"
+          />
+        </div>
     </div>
   );
 }
